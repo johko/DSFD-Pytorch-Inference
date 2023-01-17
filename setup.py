9509aca419942fc4dc2e75e6d07c9d4b8f539c3d
Johannes <johannes.kolbe@tech.better.team>
2023-01-17 12:46:11 +0100

    remove some more torch related code in setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
 import setuptools
-import torch
-import torchvision
-
-torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
-assert torch_ver >= [1, 6], "Requires PyTorch >= 1.6"
-torchvision_ver = [int(x) for x in torchvision.__version__.split(".")[:2]]
-assert torchvision_ver >= [0, 3], "Requires torchvision >= 0.3"
 
 setuptools.setup(
     name="face_detection",
     version="0.2.1",
-    author="Håkon Hukkelås",
+    author="Originally: Håkon Hukkelås, modifications by: Johannes Kolbe",
     description="A simple and lightweight package for state of the art face detection with GPU support.",
     long_description="".join(open("README.md", "r").readlines()),
     long_description_content_type="text/markdown",
-    url="https://github.com/hukkelas/DSFD-Pytorch-Inference",
+    url="https://github.com/johko/DSFD-Pytorch-Inference",
     python_requires='>=3.6',
     license="apache-2.0",
     classifiers=[
